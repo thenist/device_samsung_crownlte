0868597633586992365fa76acf202d7adbfdd5b0
Cyder <87647653+CyderVII28x03@users.noreply.github.com>
2022-06-07 14:02:05 +0700

    crownlte: Proton+ bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_crownlte.mk
+    $(LOCAL_DIR)/aosp_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_crownlte-eng \
-    lineage_crownlte-user \
-    lineage_crownlte-userdebug
+    aosp_crownlte-eng \
+    aosp_crownlte-user \
+    aosp_crownlte-userdebug
--- aosp_crownlte.mk
+++ aosp_crownlte.mk
@@ -27,11 +27,8 @@ $(call inherit-product, device/samsung/crownlte/device.mk)
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_crownlte
+PRODUCT_NAME := aosp_crownlte
 PRODUCT_DEVICE := crownlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N960F
